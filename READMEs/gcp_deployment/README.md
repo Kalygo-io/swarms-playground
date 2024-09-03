3a7c1c0fd3f0e36bbff6fe1505847c8fb1ec4090
tad dy <a@a.com>
2024-09-03 11:39:28 -0400

    fixing compile issues and adding back no-rag agent for reference

---
+++ READMEs/gcp_deployment/README.md
@@ -0,0 +1,47 @@
+# TLDR
+
+Documenting steps of how to deploy to GCP
+
+## ToC
+
+1. Artifact Registry
+2. Cloud Build
+3. Cloud Run
+
+## 1. Artifact Registry
+
+- add Dockerfile
+  - `touch Dockerfile`
+- SMOKE gcloud CLI
+  - `gcloud config list`
+  - `gcloud services enable compute.googleapis.com`
+  - `gcloud compute regions list`
+- `gcloud services enable artifactregistry.googleapis.com`
+- Create artifact in Artifact Registry
+``` template
+gcloud artifacts repositories create REPOSITORY \
+--repository-format=docker \
+--location=LOCATION \
+--description="DESCRIPTION" \
+--immutable-tags \
+--async
+```
+``` ie:
+gcloud artifacts repositories create kalygo3-nextjs \
+--repository-format=docker \
+--location=us-east1 \
+--description="Docker repository for Kalygo 3.0 web app" \
+--immutable-tags \
+--async
+```
+- CONFIRM ARTIFACT WAS CREATED: `https://console.cloud.google.com/artifacts?hl=en&project=kalygo-v3`
+  - `gcloud artifacts repositories list`
+
+## 2. Cloud Build
+
+- REFERENCE: https://cloud.google.com/build/docs/build-push-docker-image#build_an_image_using_a_build_config_file
+- `touch cloudbuild.yaml`
+- `gcloud services enable cloudbuild.googleapis.com`
+- `gcloud builds submit --region=us-central1 --config cloudbuild.yaml`
+  - REFERENCE: https://cloud.google.com/build/docs/locations#restricted_regions_for_some_projects
+   
---
+++ src/components/no-rag/chat.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ChatContext } from "@/app/dashboard/no-rag/chat-session-context";
+import { ChatList } from "@/components/no-rag/chat-list";
+import { ChatPanel } from "@/components/no-rag/chat-panel";
+import { EmptyScreen } from "@/components/no-rag/empty-screen";
+import { useScrollAnchor } from "@/shared/hooks/use-scroll-anchor";
+import { cn } from "@/shared/utils";
+import { useContext, useEffect, useState } from "react";
+
+export interface ChatProps extends React.ComponentProps<"div"> {}
+
+export function Chat({ id, className }: ChatProps) {
+  const [input, setInput] = useState("");
+  const chatState = useContext(ChatContext);
+  const { messagesRef, scrollRef, isAtBottom, scrollToBottom } =
+    useScrollAnchor();
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatState.messages, scrollToBottom]);
+
+  return (
+    <div
+      className="group w-full overflow-auto pl-0 peer-[[data-state=open]]:lg:pl-[250px] peer-[[data-state=open]]:xl:pl-[300px]"
+      ref={scrollRef}
+    >
+      <div className={cn("pb-[200px]", className)} ref={messagesRef}>
+        {chatState.messages.length ? (
+          <ChatList
+            messages={chatState.messages}
+            isCompletionLoading={chatState.completionLoading}
+          />
+        ) : (
+          <EmptyScreen />
+        )}
+      </div>
+      <ChatPanel
+        sessionId={chatState.sessionId}
+        input={input}
+        setInput={setInput}
+        isAtBottom={isAtBottom}
+        scrollToBottom={scrollToBottom}
+      />
+    </div>
+  );
+}
