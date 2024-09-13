4c2211f54b66b6dc7da6da6190e081aa10707c62
tad dy <a@a.com>
2024-09-12 23:50:49 -0400

    redirect to correct page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,5 +4,5 @@ import { protectedPageGuard } from "@/components/shared/utils/validate-token";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-  redirect("/dashboard/swarm-designer");
+  redirect("/dashboard/design-and-run");
 }
