074cefdf38b73c9e16ebb9f7761f678ac021f352
tad dy <a@a.com>
2024-09-13 04:56:12 -0400

    preparing for YouTube filming

--- src/config/navigation.tsx
+++ src/config/navigation.tsx
@@ -1,19 +1,19 @@
 import { FaDiceOne, FaDiceTwo, FaDiceThree, FaDiceFour } from "react-icons/fa";
 
 export const navigation = [
+  // {
+  //   name: "Rearrange Tool",
+  //   href: "/dashboard/rearrange",
+  //   icon: FaDiceOne,
+  // },
   {
-    name: "Rearrange Tool",
-    href: "/dashboard/rearrange",
+    name: "Design and Run Tool",
+    href: "/dashboard/design-and-run",
     icon: FaDiceOne,
   },
   {
     name: "Spreadsheet Tool",
     href: "/dashboard/spreadsheet",
     icon: FaDiceTwo,
-  },
-  {
-    name: "Design and Run Tool",
-    href: "/dashboard/design-and-run",
-    icon: FaDiceThree,
   },
 ];
