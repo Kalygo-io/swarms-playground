f663d56b5760f0377ec25a0e8fdfd106aaf0896a
tad dy <a@a.com>
2024-09-26 18:09:55 -0400

    add a version of the Swarm Builder UI for mobile screen widths

--- src/components/design-and-run/customize-swarm-drawer.tsx
+++ src/components/design-and-run/customize-swarm-drawer.tsx
@@ -8,6 +8,8 @@ import { Separator } from "@/components/shared/separator";
 import { toast } from "react-toastify";
 import { errorReporter } from "@/shared/errorReporter";
 import { validateFlow } from "@/components/design-and-run/helpers/validate-flow";
+import { callSwarmDesigner } from "@/services/callSwarmDesigner";
+import { Spinner } from "../shared/common/spinner";
 
 interface P {
   topNavHeight: number;
@@ -17,6 +19,9 @@ interface P {
 
 export default function CustomizeSwarmDrawer(P: P) {
   const [agentCount, setAgentCount] = useState(0);
+  const [swarmDesignerPrompt, setSwarmDesignerPrompt] = useState("");
+  const [callSwarmDesignerLoading, setCallSwarmDesignerLoading] =
+    useState(false);
   const { context, setSwarmDesignerContext } = useSwarmDesignerContext();
 
   const [localData, setLocalData] = useState({
@@ -25,23 +30,26 @@ export default function CustomizeSwarmDrawer(P: P) {
   });
 
   const handleAddAgent = () => {
-    if (agentCount < 10) {
-      setAgentCount(agentCount + 1);
+    if (context.agents.length < 10) {
+      setLocalData((prevData) => ({
+        ...prevData,
+        agents: [
+          ...prevData.agents,
+          {
+            name: "",
+            system_prompt: "",
+          },
+        ],
+      }));
     }
   };
 
   const handleRemoveAgent = () => {
-    if (agentCount > 0) {
-      delete localData.agents[agentCount - 1];
-
+    if (context.agents.length > 0) {
       setLocalData((prevData) => ({
         ...prevData,
-        agents: {
-          ...localData.agents,
-        },
+        agents: prevData.agents.slice(0, -1),
       }));
-
-      setAgentCount(agentCount - 1);
     }
   };
 
@@ -50,21 +58,73 @@ export default function CustomizeSwarmDrawer(P: P) {
 
     if (name.startsWith("agents.")) {
       const [_, agentKey, agentProperty] = name.split(".");
+
+      // console.log("-__-");
+      // console.log(_, agentKey, agentProperty);
+      // console.log("-__-");
+
       setLocalData((prevData) => ({
         ...prevData,
-        agents: {
-          ...prevData.agents,
-          [agentKey]: {
+        agents: [
+          ...prevData.agents.slice(0, Number.parseInt(agentKey)),
+          {
             ...prevData.agents[agentKey],
             [agentProperty]: value,
           },
-        },
+          ...prevData.agents.slice(Number.parseInt(agentKey) + 1), // Fix off by one bug
+        ],
       }));
     } else {
       setLocalData((prevData) => ({
         ...prevData,
         [name]: value,
       }));
+    }
+  };
+
+  const handleUpdateSwarmConfig = () => {
+    try {
+      console.log("handleSubmit");
+
+      validateFlow({
+        agents: localData.agents,
+        flow: localData.flow,
+      });
+
+      console.log("localData", localData);
+
+      setSwarmDesignerContext(localData);
+      toast.success("Swarm customized successfully");
+    } catch (error) {
+      errorReporter(error, true);
+    }
+  };
+
+  const handleSwarmDesignerPromptSubmit = async (
+    e: FormEvent<HTMLFormElement>
+  ) => {
+    try {
+      e.preventDefault();
+
+      setCallSwarmDesignerLoading(true);
+
+      const resp = await callSwarmDesigner(swarmDesignerPrompt);
+      setCallSwarmDesignerLoading(false);
+
+      const json = await resp.json();
+      const { swarmConfig } = json;
+
+      setLocalData({
+        agents: swarmConfig.agents,
+        flow: swarmConfig.flow,
+      });
+      setSwarmDesignerContext({
+        agents: swarmConfig.agents,
+        flow: swarmConfig.flow,
+      });
+    } catch (error) {
+      setCallSwarmDesignerLoading(false);
+      errorReporter(error, true);
     }
   };
 
@@ -102,13 +162,13 @@ export default function CustomizeSwarmDrawer(P: P) {
             >
               <form
                 className="flex h-full flex-col divide-y divide-gray-700 bg-gray-800 shadow-xl"
-                onSubmit={handleSubmit}
+                onSubmit={handleSwarmDesignerPromptSubmit}
               >
                 <div className="h-0 flex-1 overflow-y-auto">
                   <div className="bg-black px-4 py-6 sm:px-6">
                     <div className="flex items-center justify-between">
                       <DialogTitle className="text-base font-semibold leading-6 text-white">
-                        Design (Drawer)
+                        Design Swarm
                       </DialogTitle>
                       <div className="ml-3 flex h-7 items-center">
                         <button
@@ -122,16 +182,43 @@ export default function CustomizeSwarmDrawer(P: P) {
                         </button>
                       </div>
                     </div>
-                    <div className="mt-1">
-                      <p className="text-sm text-blue-300">
-                        Customize your Swarm using the form below
-                      </p>
-                    </div>
                   </div>
 
                   <div className="flex flex-1 flex-col justify-between">
                     <div className="divide-y divide-gray-200 px-4 sm:px-6">
                       <div className="space-y-6 pb-5 pt-6">
+                        <div>
+                          <div className="w-full">
+                            <div className="mt-2">
+                              <div className="flex rounded-md shadow-sm ring-gray-300 sm:max-w-md">
+                                <textarea
+                                  rows={5}
+                                  value={swarmDesignerPrompt}
+                                  onChange={(e) =>
+                                    setSwarmDesignerPrompt(e.target.value)
+                                  }
+                                  id="swarm-designer-prompt"
+                                  name="swarm-designer-prompt"
+                                  required={true}
+                                  placeholder="Describe your swarm..."
+                                  className="block w-full rounded-t-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 overflow-auto custom-scrollbar"
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div>
+                            <button
+                              type="submit"
+                              className="flex w-full justify-center rounded-b-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                            >
+                              {callSwarmDesignerLoading ? (
+                                <Spinner />
+                              ) : (
+                                "Run Swarm Builder"
+                              )}
+                            </button>
+                          </div>
+                        </div>
                         <div>
                           <div>
                             <label
@@ -141,40 +228,48 @@ export default function CustomizeSwarmDrawer(P: P) {
                               Agents
                             </label>
                             <div className="mt-2">
-                              {[...Array(agentCount)].map((_, index) => (
-                                <div key={index}>
-                                  <fieldset className="border border-gray-400 p-2 rounded-md">
-                                    <legend className="text-gray-400 text-sm px-1">
-                                      Agent {index + 1}
-                                    </legend>
-                                    {/* Rest of the code */}
+                              {localData.agents?.map(
+                                (
+                                  _: {
+                                    agent_name: string;
+                                    system_prompt: string;
+                                  },
+                                  index: number
+                                ) => (
+                                  <div key={index}>
+                                    <fieldset className="border border-gray-400 p-2 rounded-md">
+                                      <legend className="text-gray-400 text-sm px-1">
+                                        Agent {index + 1}
+                                      </legend>
+                                      {/* Rest of the code */}
 
-                                    <input
-                                      id={`agents.${index}.name`}
-                                      name={`agents.${index}.name`}
-                                      value={
-                                        localData?.agents?.[index]?.name || ""
-                                      }
-                                      onChange={handleChange}
-                                      placeholder={`Agent name`}
-                                      className="bg-gray-800 block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 mt-2"
-                                    />
-                                    <textarea
-                                      key={index}
-                                      id={`agents.${index}.system_prompt`}
-                                      name={`agents.${index}.system_prompt`}
-                                      value={
-                                        localData?.agents?.[index]
-                                          ?.system_prompt || ""
-                                      }
-                                      onChange={handleChange}
-                                      placeholder={`System prompt`}
-                                      className="bg-gray-800 block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 mt-2 resize-none h-14"
-                                    />
-                                  </fieldset>
-                                  <Separator className="my-4 bg-gray-700" />
-                                </div>
-                              ))}
+                                      <input
+                                        id={`agents.${index}.name`}
+                                        name={`agents.${index}.name`}
+                                        value={
+                                          localData?.agents?.[index]?.name || ""
+                                        }
+                                        onChange={handleChange}
+                                        placeholder={`Agent name`}
+                                        className="bg-gray-800 block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 mt-2"
+                                      />
+                                      <textarea
+                                        rows={5}
+                                        id={`agents.${index}.system_prompt`}
+                                        name={`agents.${index}.system_prompt`}
+                                        value={
+                                          localData?.agents?.[index]
+                                            ?.system_prompt || ""
+                                        }
+                                        onChange={handleChange}
+                                        placeholder={`System prompt`}
+                                        className="bg-gray-800 block w-full rounded-md border-0 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 mt-2"
+                                      />
+                                    </fieldset>
+                                    <Separator className="my-4 bg-gray-700" />
+                                  </div>
+                                )
+                              )}
                             </div>
                             <div className="flex justify-end mt-2">
                               {/* Add Agent button */}
@@ -227,7 +322,7 @@ export default function CustomizeSwarmDrawer(P: P) {
                     Cancel
                   </button>
                   <button
-                    type="submit"
+                    onClick={(e) => handleUpdateSwarmConfig()}
                     className="ml-4 inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                   >
                     Save
